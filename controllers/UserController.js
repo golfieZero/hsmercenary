fd51a7625481d84a9dc16961e5ee3b9c952fa5b1
unknown <vladislav.morozov0@inbox.ru>
2023-10-28 16:12:34 +0300

    second take

---
+++ controllers/UserController.js
@@ -0,0 +1,88 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { validationResult } from 'express-validator'
+import UserModel from '../models/user.js'
+
+
+export const login = async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ $or: [{ email: req.body.email }, { login: req.body.login }] })
+        if (!user) {
+            return res.status(400).json({
+                message: 'Неверно введён логин или пароль'
+            })
+        }
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passHash)
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверно введён логин или пароль'
+            })
+        }
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'cryptkeyverysecret',
+            {
+                expiresIn: '30d',
+            },
+        )
+        const { passHash, ...userData } = user._doc
+        res.json({
+            ...userData,
+            token
+        })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось авторизоваться',
+        })
+    }
+}
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+        // передаём данные в бд
+        const doc = new UserModel({
+            login: req.body.login,
+            passHash: hash,
+            email: req.body.email,
+            avatarUrl: req.body.avatarUrl
+        });
+        const user = await doc.save()
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'cryptkeyverysecret',
+            {
+                expiresIn: '30d',
+            },
+        )
+        const { passHash, ...userData } = user._doc
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться',
+        })
+    }
+}
+
+export const profile = async (req, res) => {
+    try {
+
+    }
+    catch (err) {
+
+    }
+}
--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
+import * as UserController from './controllers/UserController.js'
+import { registerValidation } from './validations/auth.js'
+
 const PORT = 8000;
 const app = express();
-// подключаем валидацию
-//const { registerValidation } = require('./validations/auth.js')
-import { registerValidation } from './validations/auth.js'
-import { validationResult } from 'express-validator'
-
-// Импортируем модель пользователя
-import UserModel from './models/user.js'
 //подключаем бд
 mongoose
     .connect('mongodb+srv://golfiedev:golfiedev@cluster0.qyyqdfs.mongodb.net/test?retryWrites=true&w=majority')
     .then(() => console.log('Db connected'))
-    .catch((err) => console.log('db error', err))
+    .catch((err) => console.log('db error:', err))
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, async (req, res) =>{
-    const errors = validationResult(req)
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors.array())
-    }
-//Шифруем пароль методом salt
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const passHash = await bcrypt.hash(password, salt)
-// передаём данные в бд
-    const doc = new UserModel({
-        login: req.body.login,
-        passHash,   
-        email: req.body.email,
-        avatarUrl: req.body.avatarUrl
-    });
-    const user = await doc.save()
-    res.json(user)
-    })
+app.post('/auth/login', UserController.login)
+
+app.post('/auth/register', registerValidation, UserController.register)
+
+app.get('/auth/me', UserController.login)
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порте ${PORT}`);
-  });
+});
